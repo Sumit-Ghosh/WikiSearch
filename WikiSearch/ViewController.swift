4887af6291750907da972695f7967891630499fd
sumit_ghosh <sumit.ghoshqa@gmail.com>
2020-03-13 01:58:35 +0530

    Initial Commit

---
+++ WikiSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiSearch
+//
+//  Created by Sumit Ghosh on 13/03/20.
+//  Copyright © 2020 Sumit Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
